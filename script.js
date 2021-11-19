d59d51a708a7c7faa1c5df8eec88f5e8086a1c98
evieh <evholyoake@gmail.com>
2021-11-19 09:19:40 -0600

    files for html page layout
    Added a mock front page to show functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function() {
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+  };
+}
+
+
