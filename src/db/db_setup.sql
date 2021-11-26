5b834a400c480e8f99dd71e406388f513d012f89
Alex-R-Brown <alexander-r-brown@uiowa.edu>
2021-11-26 16:01:11 -0600

    Fixing merge conflicts.

--- src/db/db_setup.sql
+++ src/db/db_setup.sql
@@ -1,22 +1,16 @@
 /* DROP ALL TABLES */
-<<<<<<< HEAD
-<<<<<<< HEAD
-DROP TABLE admins;
-DROP TABLE schedules;
-DROP TABLE invites;
-DROP TABLE IF EXISTS slots; 
-DROP TABLE IF EXISTS reservations;
-=======
-=======
->>>>>>> bb1011c6abddbf40d57bcec544100235da6fcbc7
 DROP TABLE IF EXISTS admins;
 DROP TABLE IF EXISTS schedules;
 DROP TABLE IF EXISTS invites;
+DROP TABLE IF EXISTS slots; 
+DROP TABLE IF EXISTS reservations;
+
 
 /* RECREATE ALL TABLES */
 CREATE TABLE admins (
     username VARCHAR(16) PRIMARY KEY,
     password VARCHAR(16) NOT NULL
 );
+
 
 /* SEED TABLES */
